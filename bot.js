2ef9382704dbf9659bfef8d85e1cf963843ab35d
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2018-12-01 04:43:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const client2 = new Discord.Client();
 client.on('ready', () => {
   
 client.user.setGame("Clan Strict ..", "https://twitch.tv/idk");
-client2.user.setGame("v1.", "https://twitch.tv/idk");
+client2.user.setGame("V1.", "https://twitch.tv/idk");
    console.log(`----------------`);
    console.log(`Credit Farmm - Script By : Kahrbaa `);
    console.log(`----------------`);
