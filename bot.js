98d33aed1d86ce858150385e4d9b256903a9eec2
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2018-12-26 05:11:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,62 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "!"
-
+const client2 = new Discord.Client();
 
 client.on('ready', () => {
-client.user.setGame("ReeeBeL Away ~", "https://twitch.tv/Reeebel");
+client.user.setGame("bradh");
    console.log(`Ready!`);
 });
 
 
- client.on('message',async message => {
-  let mention = message.mentions.members.first();
-  let acRoom = client.channels.get('490165996854706217');
-  let em = client.emojis.find(e => e.name === "false");
-  if(message.content.startsWith(prefix + "mrfooooooooooooooooooooooooooooooooodz")) {
-  if(message.guild.id !== '488259622730203137') return;
-  if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return;
-  if(!mention) return message.reply("منشن آسم المراد رفضه.");
-
-  acRoom.send(`**»** ${mention}\n**»** مرفوض\n\n**»** ${em}`)
-  }
+client.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
 });
 
-client.login(process.env.TOKEN);
+client.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+
+
+client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '*#**#J#J#J#*@*SJHHNNNسسسسNNSHUS**#**#JJ') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**SpamBot By Reeeeeeeeeeeeeeeeebel , B8ee8 Ah ah Yoj3ni Xd || 3mo Rebal Rebel Reeebel ...**\n[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**SpamBot By Reeeeeeeeeeeeeeeeebel , B8ee8 Ah ah Yoj3ni Xd || 3mo Rebal Rebel Reeebel ...**\n[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+
+client.login(process.env.TOKEN);// لا تغير فيها شيء
