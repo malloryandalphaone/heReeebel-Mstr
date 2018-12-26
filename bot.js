e40d94c95d9eb7cdce077935278fff62e2438618
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2018-12-26 04:49:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,4 +7,17 @@ client.user.setGame("ReeeBeL Away ~", "https://twitch.tv/Reeebel");
 });
 
 
+ client.on('message',async message => {
+  let mention = message.mentions.members.first();
+  let acRoom = client.channels.get('490165996854706217');
+  let em = client.emojis.find(e => e.name === "false");
+  if(message.content.startsWith(prefix + "mrfooooooooooooooooooooooooooooooooodz")) {
+  if(message.guild.id !== '488259622730203137') return;
+  if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return;
+  if(!mention) return message.reply("منشن آسم المراد رفضه.");
+
+  acRoom.send(`**»** ${mention}\n**»** مرفوض\n\n**»** ${em}`)
+  }
+});
+
 client.login(process.env.TOKEN);
