0cc9d0332833e9fed16817e19bca96bf34fe8223
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2018-12-29 14:47:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,62 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const client2 = new Discord.Client();
 
 client.on('ready', () => {
-client.user.setGame("bradh");
+client.user.setGame("I Hate the life.", "LISTENING");
    console.log(`Ready!`);
 });
 
 
 client.on('message', message => {
-    if(message.content === '-راتب'){
+    if(message.content === 'يوم'){
         message.channel.send('#daily')
     }
 });
-
-client.on('message', message => {
-    if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
-    }
-});
-
-
-
-client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '*#**#J#J#J#*@*SJHHNNNسسسسNNSHUS**#**#JJ') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**SpamBot By Reeeeeeeeeeeeeeeeebel , B8ee8 Ah ah Yoj3ni Xd || 3mo Rebal Rebel Reeebel ...**\n[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**SpamBot By Reeeeeeeeeeeeeeeeebel , B8ee8 Ah ah Yoj3ni Xd || 3mo Rebal Rebel Reeebel ...**\n[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-
 
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
